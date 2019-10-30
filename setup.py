bb4465f709f13da73384c9e85f32f8b05a5dfd4b
Nuno Miguel Casteloa da Silva <NunoMCSilva@gmail.com>
2019-10-30 04:25:22 +0000

    Add basic setup.py

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@
 from distutils.core import setup
 
 setup(
-      name='bisca',
-      version='0.0.1',
-      # TODO: description='',
-      # TODO: long_description=
-      author='Nuno Miguel Casteloa da Silva',
-      # TODO: author_email='gward@python.net',
-      # TODO: url='https://www.python.org/sigs/distutils-sig/',
-      packages=['bisca'],     # TODO: , tests?
-      # TODO: classifiers, platforms, keywords, license
+    name="bisca",
+    version="0.0.1",
+    # TODO: description='',
+    # TODO: long_description=
+    author="Nuno Miguel Casteloa da Silva",
+    # TODO: author_email='gward@python.net',
+    # TODO: url='https://www.python.org/sigs/distutils-sig/',
+    packages=["bisca"],  # TODO: , tests?
+    # TODO: classifiers, platforms, keywords, license
 )
