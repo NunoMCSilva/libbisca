051179be3bc072a4a53d0864d9346c100686be18
Nuno Miguel Casteloa da Silva <NunoMCSilva@gmail.com>
2019-10-30 01:22:11 +0000

    Initial commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+# TODO #!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+      name='bisca',
+      version='0.0.1',
+      # TODO: description='',
+      # TODO: long_description=
+      author='Nuno Miguel Casteloa da Silva',
+      # TODO: author_email='gward@python.net',
+      # TODO: url='https://www.python.org/sigs/distutils-sig/',
+      packages=['bisca'],     # TODO: , tests?
+      # TODO: classifiers, platforms, keywords, license
+)
