ee5e6e53dcb8606529da084acd8c716b5df06b92
Nuno Miguel Casteloa da Silva <NunoMCSilva@gmail.com>
2019-10-30 03:47:19 +0000

    Add state and test_state

---
+++ bisca/state.py
@@ -0,0 +1,157 @@
+# TODO: add docstrings
+
+from enum import IntEnum
+from typing import Optional, Tuple
+
+# TODO: relative imports
+from bisca.card import Card
+
+
+class Player(IntEnum):
+    NORTH = 1
+    SOUTH = -1
+
+
+class Winner(IntEnum):
+    NORTH = 1
+    DRAW = 0
+    SOUTH = -1
+
+
+class State:
+    NUM_PLAYERS = 2  # code only handles 2 players (for now, and maybe in the future)
+    HAND_SIZE = 3  # TODO: extend code to Bisca9 later
+
+    def __init__(self, eldest: Player = Player.SOUTH):
+        self.turn = eldest
+        self.stock = Card.get_deck()
+        self.table = []
+
+        self.hands = {player: [] for player in Player}
+        # self.piles seems unnecessary
+
+        # can be reduced to a single number, but this is simpler
+        self.scores = {player: 0 for player in Player}
+
+        # handling trumps
+        self.stock = [
+            (card.get_trumped() if card.suit == self.stock[0].suit else card)
+            for card in self.stock
+        ]
+        self.trump = self.stock[0]
+
+        # deal initial hand
+        for _ in range(self.HAND_SIZE):
+            self._deal_cards()
+
+    @property
+    def score(self) -> Optional[int]:
+        if self.is_endgame():
+            assert sum(self.scores.values()) == 120
+
+            if self.winner == Winner.DRAW:
+                return 60
+            else:
+                return self.scores[self.winner]
+        else:
+            return None
+
+    @property
+    def winner(self) -> Optional[Winner]:
+        # TODO: or just raise ValueError if it's called before is_terminal is True
+
+        if self.is_endgame():
+            if self.scores[Player.NORTH] == self.scores[Player.SOUTH]:
+                return Winner.DRAW
+            else:
+                return Winner.NORTH if self.scores[Player.NORTH] > self.scores[Player.SOUTH] else Winner.SOUTH
+        else:
+            return None     # raise ValueError("game not yet finished") -- improve exception type and msg
+
+    def is_endgame(self) -> bool:
+        return not (bool(self.stock) and bool(self.hands[Player.NORTH]) and bool(self.hands[Player.SOUTH]))
+
+    def play(self, move: Card) -> Optional[Tuple[Player, int]]:
+        # TODO: recheck game rules (sp. that bit about follow after empty stock -- will need to modify __gt__ for that)
+
+        eldest = self.table == []
+
+        # TODO: better exception here would be IllegalMove?
+        self.hands[self.turn].remove(move)
+
+        # TODO: better format other than (player, card)
+        self.table.append((self.turn, move))
+
+        if eldest:
+            self.turn = -self.turn
+        else:
+            # youngest
+            winner = self._get_round_winner()
+            added_score = sum(card.score for _, card in self.table)
+
+            self.scores[winner] += added_score
+            self.turn = winner
+            self.table = []
+
+            # if stock exists, deal cards -- TODO: put in deal cards? (ignores call if stock doesn't exist)
+            if self.stock:
+                self._deal_cards()
+
+            return winner, added_score
+
+        return None
+
+    def _deal_cards(self) -> None:
+        for turn in (self.turn, -self.turn):
+            self.hands[turn].append(self.stock.pop())
+
+    def _get_round_winner(self) -> Player:
+        (player1, card1), (player2, card2) = self.table
+        return player1 if card1 > card2 else player2
+
+
+
+"""
+
+
+
+
+
+
+    def __repr__(self):
+        # TODO: refactor -- turn, hands, winner and score need a better representation
+        return (
+            f"State("
+            f"\n\tturn = {self.turn}, "
+            f"\n\tstock = {self.stock}, "
+            f"\n\ttrump = {self.trump}, "
+            f"\n\thands = {self.hands}, "
+            f"\n\tscores = {self.scores}, "
+            f"\n\ttable = {self.table}"
+            f"\n\tis_endgame = {self.is_endgame()}"
+            f"\n\twinner = {self.winner}"
+            f"\n\tscore == {self.score}"
+            f"\n)"
+        )
+
+    # TODO: remove _is_endgame, _winner, etc. (or make it work--check that) -- is not working as well as I want and it is redundant
+
+
+    # TODO: add save/load? -- to json
+
+
+
+
+
+
+""
+class State:
+    __init__(eldest=0)
+    __repr__()
+    is_endgame()
+    play(move)
+    winner()
+""
+
+
+"""
---
+++ tests/unit/test_state.py
@@ -0,0 +1,187 @@
+# -*- coding: utf-8 -*-
+
+# TODO: add docstrings
+
+from bisca.card import Card, Rank, Suit
+from bisca.state import State, Player, Winner
+
+
+def test__state_init__first_eldest_is_south_player__initializes_correctly(mocker):
+    # arrange
+    expected_stock = (
+        "[A♡, 2♡, 3♡, 4♡, 5♡, 6♡, 7♡, Q♡, J♡, K♡, A♦, 2♦, 3♦, 4♦, 5♦, 6♦, 7♦, Q♦, J♦, K♦, A♠, 2♠, 3♠, "
+        "4♠, 5♠, 6♠, 7♠, Q♠, J♠, K♠, A♣, 2♣, 3♣, 4♣]"
+    )
+    mocker.patch("random.SystemRandom.shuffle")
+
+    # act
+    state = State()
+
+    # assert
+    assert str(state.stock) == expected_stock
+    assert state.turn == Player.SOUTH
+    assert repr(state.trump) == "A♡"
+    assert repr(state.hands[Player.SOUTH]) == "[K♣, Q♣, 6♣]"
+    assert repr(state.hands[Player.NORTH]) == "[J♣, 7♣, 5♣]"
+    assert state.scores == {Player.SOUTH: 0, Player.NORTH: 0}
+    assert state.table == []
+
+
+def test__state_play__play_eldest__runs_correct(mocker):
+    # method is command method, check modifications
+
+    # arrange
+    expected_stock = (
+        "[A♡, 2♡, 3♡, 4♡, 5♡, 6♡, 7♡, Q♡, J♡, K♡, A♦, 2♦, 3♦, 4♦, 5♦, 6♦, 7♦, Q♦, J♦, K♦, A♠, 2♠, 3♠, "
+        "4♠, 5♠, 6♠, 7♠, Q♠, J♠, K♠, A♣, 2♣, 3♣, 4♣]"
+    )
+    mocker.patch("random.SystemRandom.shuffle")
+    state = State()
+
+    # TODO: could use a Card.get_card("KC") for testing
+    move = Card(Rank.KING, Suit.CLUBS)
+
+    # act
+    result = state.play(move)
+
+    # assert
+    assert result is None
+    assert str(state.stock) == expected_stock
+    assert state.turn == Player.NORTH
+    assert repr(state.trump) == "A♡"
+    assert repr(state.hands[Player.SOUTH]) == "[Q♣, 6♣]"
+    assert repr(state.hands[Player.NORTH]) == "[J♣, 7♣, 5♣]"
+    assert state.scores == {Player.SOUTH: 0, Player.NORTH: 0}
+
+    # TODO: this is an internal implementation issue... hide?
+    assert state.table == [(Player.SOUTH, move)]
+
+
+def test__state_play__play_youngest__runs_correct(mocker):
+    # method is command method, check modifications
+
+    # arrange
+    expected_stock = (
+        "[A♡, 2♡, 3♡, 4♡, 5♡, 6♡, 7♡, Q♡, J♡, K♡, A♦, 2♦, 3♦, 4♦, 5♦, 6♦, 7♦, Q♦, J♦, K♦, A♠, 2♠, 3♠, "
+        "4♠, 5♠, 6♠, 7♠, Q♠, J♠, K♠, A♣, 2♣]"
+    )
+    mocker.patch("random.SystemRandom.shuffle")
+
+    state = State()
+
+    # TODO: could use a Card.get_card("KC") for testing
+    move1 = Card(Rank.KING, Suit.CLUBS)
+
+    move2 = Card(Rank.SEVEN, Suit.CLUBS)
+    state.play(move1)
+
+    # act
+    result = state.play(move2)
+
+    # assert
+    assert result == (Player.NORTH, 14)  # South: KC vs North: 7C -> North, 14
+    assert str(state.stock) == expected_stock
+    assert state.turn == Player.NORTH
+    assert repr(state.trump) == "A♡"
+    assert repr(state.hands[Player.SOUTH]) == "[Q♣, 6♣, 3♣]"
+    assert repr(state.hands[Player.NORTH]) == "[J♣, 5♣, 4♣]"
+    assert state.table == []
+
+    # TODO: need to check it adds and not just replaces
+    assert state.scores == {Player.SOUTH: 0, Player.NORTH: 14}
+
+
+def test__state_is_endgame__initial_state__return_false():
+    # arrange
+    state = State()
+
+    # act & assert
+    assert state.is_endgame() is False
+
+
+def test__state_is_endgame__final_state__return_false():
+    # arrange
+    state = State()
+
+    state.stock = []
+    state.hands = {player: [] for player in Player}
+    state.scores = {player: 60 for player in Player}
+
+    # act & assert
+    assert state.is_endgame() is True
+
+
+def test__state_winner__initial_state__return_none():
+    # arrange
+    state = State()
+
+    # act & assert
+    assert state.winner is None
+
+
+def test__state_winner__draw__return_winner_draw():
+    # arrange
+    state = State()
+
+    state.stock = []
+    state.hands = {player: [] for player in Player}
+    state.scores = {player: 60 for player in Player}
+
+    # act & assert
+    assert state.winner == Winner.DRAW
+
+
+def test__state_winner__north_win__return_winner_north():
+    # arrange
+    state = State()
+
+    state.stock = []
+    state.hands = {player: [] for player in Player}
+    state.scores = {Player.NORTH: 90, Player.SOUTH: 30}
+
+    # act & assert
+    assert state.winner == Winner.NORTH
+
+
+def test__state_winner__south_win__return_winner_north():
+    # arrange
+    state = State()
+
+    state.stock = []
+    state.hands = {player: [] for player in Player}
+    state.scores = {Player.NORTH: 30, Player.SOUTH: 90}
+
+    # act & assert
+    assert state.winner == Winner.SOUTH
+
+
+def test__state_score__initial_state__return_none():
+    # arrange
+    state = State()
+
+    # act & assert
+    assert state.score is None
+
+
+def test__state_score__draw__return_winner_draw():
+    # arrange
+    state = State()
+
+    state.stock = []
+    state.hands = {player: [] for player in Player}
+    state.scores = {player: 60 for player in Player}
+
+    # act & assert
+    assert state.score == 60
+
+
+def test__state_score__normal_win__return_winner_north():
+    # arrange
+    state = State()
+
+    state.stock = []
+    state.hands = {player: [] for player in Player}
+    state.scores = {Player.NORTH: 90, Player.SOUTH: 30}
+
+    # act & assert
+    assert state.score == 90
