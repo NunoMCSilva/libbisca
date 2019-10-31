e0da27195e0438db541c3620d3c9a048e25f9a31
Nuno Miguel Casteloa da Silva <NunoMCSilva@gmail.com>
2019-10-31 17:36:32 +0000

    Refactor card and test_card

--- libbisca/card.py
+++ libbisca/card.py
@@ -5,17 +5,8 @@ from enum import Enum
 from random import SystemRandom
 from typing import List
 
-RANK_ORDER = "23456QJK7A"
-RANK_SCORE = {
-    "Q": 2,
-    "J": 3,
-    "K": 4,
-    "7": 10,
-    "A": 11,
-}  # all other ranks are worth 0 points
 
-
-# TODO: check about how to add RANK_ORDER and RANK_SCORE to Enum instead of it being outside
+# Rank =================================================================================================================
 class Rank(Enum):
     ACE = "A"
     TWO = "2"
@@ -29,13 +20,19 @@ class Rank(Enum):
     KING = "K"
 
     def __gt__(self, other: "Rank"):
-        return RANK_ORDER.index(self.value) > RANK_ORDER.index(other.value)
+        # TODO: ok, this might be improved...
+        return self._ORDER.index(self.value) > self._ORDER.index(other.value)
 
     @property
     def score(self) -> int:
-        return RANK_SCORE.get(self.value, 0)
+        return self._SCORE.get(self.value, 0)
 
 
+Rank._ORDER = "23456QJK7A"
+Rank._SCORE = {"Q": 2, "J": 3, "K": 4, "7": 10, "A": 11}
+
+
+# Suit =================================================================================================================
 class Suit(Enum):
     HEARTS = "♥"
     DIAMONDS = "♦"
@@ -43,6 +40,7 @@ class Suit(Enum):
     CLUBS = "♣"
 
 
+# Card =================================================================================================================
 @dataclass
 class Card:
     rank: Rank
@@ -52,10 +50,11 @@ class Card:
         return hash((self.rank, self.suit))
 
     def __gt__(self, other: "Card"):
-        # only gt is implemented, others aren't necessary
+        # only gt is implemented, the others aren't really necessary
         return False if other.is_trump() else self.rank > other.rank
 
     def __repr__(self):
+        #  This should be in __str__ but I want to print a list of cards and seen this -- TODO: improve this
         # "Although practicality beats purity." -- The Zen of Python
         return f"{self.rank.value}{self.suit.value}"
 
@@ -64,25 +63,13 @@ class Card:
         return self.rank.score
 
     @staticmethod
-    def get_deck(shuffle: bool = True) -> List["Card"]:
-        # TODO: put deck at module level to save memory?
-        deck = [Card(rank, suit) for suit in Suit for rank in Rank]
-
-        if shuffle:
-            # SystemRandom is necessary to generate all of the 40! possibilities
-            SystemRandom().shuffle(deck)
-
-        return deck
-
-    def get_trumped(self) -> "TrumpCard":
-        return TrumpCard(self.rank, self.suit)
-
-    @staticmethod
     def is_trump() -> bool:
         return False
 
 
+# TrumpCard ============================================================================================================
 class TrumpCard(Card):
+    # TODO: this might be better implemented in something TrumpSuit or...
     TRUMP_SUIT_STR = {
         Suit.HEARTS: "♡",
         Suit.DIAMONDS: "♢",
@@ -94,12 +81,35 @@ class TrumpCard(Card):
         return self.rank > other.rank if other.is_trump() else True
 
     def __repr__(self):
+        #  This should be in __str__ but I want to print a list of cards and seen this -- TODO: improve this
         # "Although practicality beats purity." -- The Zen of Python
         return f"{self.rank.value}{TrumpCard.TRUMP_SUIT_STR[self.suit]}"
-
-    def get_trumped(self) -> "TrumpCard":
-        raise NotImplementedError
 
     @staticmethod
     def is_trump() -> bool:
         return True
+
+
+# Deck =================================================================================================================
+class Deck:
+
+    def __init__(self, shuffle=True):
+        # TODO: replace by module level _DECK (memory saves) and do copy or something? [or change __deepcopy__ in Card?]
+        deck = [Card(rank, suit) for suit in Suit for rank in Rank]
+
+        if shuffle:
+            # SystemRandom is necessary to generate all of the 40! possibilities
+            SystemRandom().shuffle(deck)
+
+        # set trumps
+        self.deck = [(TrumpCard(card.rank, card.suit) if card.suit == deck[0].suit else card) for card in deck]
+
+    def __str__(self):
+        return str(self.deck)
+
+    @property
+    def bottom(self) -> Card:
+        return self.deck[0]
+
+    def pop(self) -> Card:
+        return self.deck.pop()
--- tests/unit/test_card.py
+++ tests/unit/test_card.py
@@ -2,7 +2,7 @@
 
 import pytest
 
-from libbisca.card import Card, Rank, Suit, TrumpCard
+from libbisca.card import *
 
 
 # TODO: check black's change to this comments formatting... don't really like it
@@ -10,24 +10,31 @@ from libbisca.card import Card, Rank, Suit, TrumpCard
 @pytest.mark.parametrize(
     "card1, card2, expected",
     [
-        # same type, same suit: compare rank
+        # same type, same suit: compare rank ---------------------------------------------------------------------------
+
         # AH > 7H
         (Card(Rank.ACE, Suit.HEARTS), Card(Rank.SEVEN, Suit.HEARTS), True),
         (TrumpCard(Rank.ACE, Suit.HEARTS), TrumpCard(Rank.SEVEN, Suit.HEARTS), True),
+
         # 5C < 7C
         (Card(Rank.FIVE, Suit.CLUBS), Card(Rank.SEVEN, Suit.CLUBS), False),
         (TrumpCard(Rank.FIVE, Suit.CLUBS), TrumpCard(Rank.SEVEN, Suit.CLUBS), False),
-        # same type, different suits: compare rank -- in this libbisca variant
+
+        # same type, different suits: compare rank -- in this libbisca variant -----------------------------------------
         # TODO: check about other libbisca variants (at least one mandates follow after stock is empty)
+
         # AC > 7H
         (Card(Rank.ACE, Suit.CLUBS), Card(Rank.SEVEN, Suit.HEARTS), True),
         (TrumpCard(Rank.ACE, Suit.CLUBS), TrumpCard(Rank.SEVEN, Suit.HEARTS), True),
+
         # 5D < 7C
         (Card(Rank.FIVE, Suit.DIAMONDS), Card(Rank.SEVEN, Suit.CLUBS), False),
         (TrumpCard(Rank.FIVE, Suit.DIAMONDS), TrumpCard(Rank.SEVEN, Suit.CLUBS), False),
-        # different type
+
+        # different type -----------------------------------------------------------------------------------------------
         # 2D(t) > 7C
         (TrumpCard(Rank.TWO, Suit.DIAMONDS), Card(Rank.SEVEN, Suit.CLUBS), True),
+
         # 3D < 2S(t)
         (Card(Rank.THREE, Suit.DIAMONDS), TrumpCard(Rank.TWO, Suit.SPADES), False),
     ],
@@ -37,29 +44,15 @@ def test__card_and_trump_card_gt__two_cards__return_expected(card1, card2, expec
     assert (card1 > card2) is expected
 
 
-def test__card_get_deck__shuffle_false__return_expected():
+def test__deck_init__shuffle_false__return_expected():
     # arrange
     expected = (
-        "[A♥, 2♥, 3♥, 4♥, 5♥, 6♥, 7♥, Q♥, J♥, K♥, A♦, 2♦, 3♦, 4♦, 5♦, 6♦, 7♦, Q♦, J♦, K♦, A♠, 2♠, 3♠, 4♠, 5♠,"
+        "[A♡, 2♡, 3♡, 4♡, 5♡, 6♡, 7♡, Q♡, J♡, K♡, A♦, 2♦, 3♦, 4♦, 5♦, 6♦, 7♦, Q♦, J♦, K♦, A♠, 2♠, 3♠, 4♠, 5♠,"
         " 6♠, 7♠, Q♠, J♠, K♠, A♣, 2♣, 3♣, 4♣, 5♣, 6♣, 7♣, Q♣, J♣, K♣]"
     )
 
     # act
-    deck = Card.get_deck(shuffle=False)
+    deck = Deck(shuffle=False)
 
     # assert
     assert str(deck) == expected
-
-
-def test__card_get_trumped__any_card__returns_trump_card_version():
-    # arrange
-    card = Card(Rank.SEVEN, Suit.CLUBS)
-
-    # act
-    trump_card = card.get_trumped()
-
-    # assert -- TODO: improve this
-    assert isinstance(trump_card, TrumpCard)
-    assert trump_card.is_trump()
-    assert repr(trump_card) == "7♧"
-    assert trump_card > card
