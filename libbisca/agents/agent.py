2e5bb7a4243aa1fea9869951754b49aabf0da460
Nuno Miguel Casteloa da Silva <NunoMCSilva@gmail.com>
2019-11-01 05:56:20 +0000

    Add agent.py

---
+++ libbisca/agents/agent.py
@@ -0,0 +1,23 @@
+# TODO: add docstrings
+
+from abc import ABC, abstractmethod
+import random
+
+from libbisca.agents.dev import ObservedState
+from libbisca.card import Card
+
+
+class Agent(ABC):
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}()"
+
+    @abstractmethod
+    def get_move(self, state: ObservedState) -> Card:
+        raise NotImplementedError
+
+
+class RandomAgent(Agent):
+
+    def get_move(self, state: ObservedState) -> Card:
+        return random.choice(state.hand)
