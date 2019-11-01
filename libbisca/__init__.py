e4aed2f57637694d6250f7d2b4cf351c66f4550c
Nuno Miguel Casteloa da Silva <NunoMCSilva@gmail.com>
2019-11-01 13:23:47 +0000

    Small changes

--- libbisca/__init__.py
+++ libbisca/__init__.py
@@ -1,11 +1,11 @@
-"""Bisca card game library"""
-# TODO: complete docstring
+"""Bisca card game library"""   # TODO: complete docstring
 
 from libbisca.agents import Agent, RandomAgent, ObservedState, PossibleCards, Runner
 from libbisca.card import Card, Deck
 from libbisca.game import Game
 from libbisca.state import State, Player, Winner
 
+__version__ = "0.0.1"
 __all__ = [
     "Agent",
     "RandomAgent",
@@ -19,4 +19,3 @@ __all__ = [
     "Player",
     "Winner",
 ]
-__version__ = "0.0.1"
