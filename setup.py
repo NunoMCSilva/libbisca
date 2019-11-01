67d200dcfc760f5e966f0773e1d7e487ba095e8f
Nuno Miguel Casteloa da Silva <NunoMCSilva@gmail.com>
2019-11-01 14:38:02 +0000

    Workingm

--- setup.py
+++ setup.py
@@ -1,27 +1,42 @@
 import pathlib
 from setuptools import setup, find_packages
 
-HERE = pathlib.Path(__file__).parent
 
+def get_version():
+    for line in (HERE / "libbisca" / "__init__.py").read_text().split("\n"):
+        if line.startswith("__version__"):
+            return line.split('"')[1]
+
+
+HERE = pathlib.Path(__file__).parent
 README = (HERE / "README.md").read_text()
+VERSION = get_version()
+
 
 setup(
     name="libbisca",
-    version="0.0.1",
+    version=VERSION,
     description="Bisca card game library",
     long_description=README,
     long_description_content_type="text/markdown",
-    # TODO: url="https://github.com/",
+    url="https://github.com/NunoMCSilva/libbisca",
     author="Nuno Miguel Casteloa da Silva",
     author_email="NunoMCSilva@gmail.com",
-    # TODO: license="",
+    license="GPL-3.0",
     classifiers=[
-        # TODO: add classifiers
+        "Development Status :: 2 - Pre-Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Topic :: Games/Entertainment",
+        "Topic :: Software Development :: Libraries :: Python Modules",
     ],
-    packages=find_packages(exclude=("tests",)),  # TODO: not sure about this...
-    # include_package_data=True,
-    # TODO: what about tests?
-    # install_requires=[],   # TODO: install_requires? check this...
-    # TODO: doesn't have entry_points?
+    keywords="bisca, cards, card game",
+    packages=find_packages(),
+    python_requires=">=3.7",
+    install_requires=[],
+    tests_require=["pytest>=5.2.2", "pytest-mock>=1.11.2"],
 )
-# TODO: add keywords, platform?
