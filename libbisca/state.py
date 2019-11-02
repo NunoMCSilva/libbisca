175c28fe752fc4a4aef7ca31ee4b4126e8c46094
Nuno Miguel Casteloa da Silva <NunoMCSilva@gmail.com>
2019-11-02 06:56:40 +0000

    Make lots of changes and apply Black

--- libbisca/state.py
+++ libbisca/state.py
@@ -1,187 +1,196 @@
 """Bisca State
 
-Implements the rules for a single variant.
+Implements the rules for a single Bisca variant.
 
 Exports the following classes:
     * Player - enum representing the two players (North and South)
-    * Winner - enum representing the win conditions (North win, South win and Draw)
+    * Winner - enum representing endgame results (North win, South win and Draw)
     * State - a snapshot of the current game state
 """
 
-from enum import IntEnum
-from typing import Optional, Tuple
+from abc import ABC, abstractmethod
+import copy
+from enum import Enum, IntEnum
+from typing import List, Optional, Tuple
 
 from libbisca.card import Card, Deck
 
 
 class Player(IntEnum):
+    # choice of value is due to SOUTH being the player character in a future GUI (not part of library)
     NORTH = -1
-    SOUTH = 1  # player side on future GUI
+    SOUTH = 1
+
+    def __format__(self, format_spec):
+        return "North" if self == Player.NORTH else "South"
+
+    @property
+    def opponent(self) -> "Player":
+        return Player.SOUTH if self == Player.NORTH else Player.NORTH
 
 
 class Winner(IntEnum):
-    SOUTH = Player.SOUTH
-    DRAW = 0
+    # endgame win
     NORTH = Player.NORTH
+    DRAW = 0
+    SOUTH = Player.SOUTH
+
+    def __format__(self, format_spec):
+        # TODO: need to join to Player __format__
+        return (
+            "Draw"
+            if self == Winner.DRAW
+            else ("North" if self == Player.NORTH else "South")
+        )
 
 
-class State:
-    """
-    Represents a game state (snapshot)
+class HandSize(Enum):
+    # this prevents non-approved/tested value from being used
+    THREE = 3
+    # SEVEN = 7 -- TODO: to test
+    # NINE = 9 -- TODO: to test
 
-    Attributes
-    ----------
-    NUM_PLAYERS : int
-        num of players
-    HAND_SIZE : int
-        num of cards in hand
-    turn : Player
-        game turn
-    stock : List[Card]
-        un-dealt stock of cards
-    trump : Card
-        game trump
-    hands : Dict[Player, Card]
-        player cards in hands
-    table : List[Card]
-        cards on the table
-    score : int
-        score from the pov of the South player: (South score - North score)
 
-    Methods
-    -------
-    do_rollout()
-        continue game with random moves until endgame (useful for Monte Carlo agents)
-    get_score(player)
-        return score of given player
-    is_endgame()
-        return True if this is a endgame/terminal state, False otherwise
-    play(move)
-        modify state by playing the given card
-    opponent
-        return player currently not in turn
-    winner
-        return winner of game on endgame state, None otherwise
+class State(ABC):
+    """Represents a game state (snapshot)
+
+    # TODO: complete docstring (check google recs first)
     """
 
-    NUM_PLAYERS = 2  # code will only handle 2 player for now (and maybe in the future)
-    HAND_SIZE = 3  # TODO: extend code to Bisca9 later
+    NUM_PLAYERS = 2  # for now, code will only handle 2 players
 
-    def __init__(self, eldest: Player = Player.SOUTH, hand_size: int = HAND_SIZE):
-        """
-        Parameters
-        ----------
-        eldest : Player
-            first player to move
-        hand_size : int
-            player hand size (3, 7 ou 9)
-        """
+    def __init__(
+        self, eldest: Player = Player.SOUTH, hand_size: HandSize = HandSize.THREE
+    ):
+        self.hand_size = hand_size.value
+
         self.turn = eldest
 
         self.stock = Deck()
         self.trump = self.stock.bottom
 
         self.hands = {player: [] for player in Player}
-        # TODO: substitute (player, card) so table is accessible without revealing inner details
+        self._scores = {player: 0 for player in Player}
+        # self.piles isn't useful
+
         self.table = []
-        self._table_player = []
+        self._table_played = []
 
-        # this is from the POV of Player.SOUTH
-        self.score = 0
-        self._scores = {
-            player: 0 for player in Player
-        }  # TODO: ok, I need to get the score system to work better
-
-        # this is for a more efficient is_endgame - TODO: add len(self.stock)?
-        self._cards_in_stock_and_hands = 40
+        self._cards_in_stock_and_hands = len(self.stock)
 
         # deal initial hand
-        for _ in range(self.HAND_SIZE):
+        for _ in range(self.hand_size):
             self._deal_cards()
 
-    # TODO: is __hash__(self): necessary?
+    # TODO: __hash__?
 
-    def __str__(self):
-        # TODO: improve this (pretty print?)
+    def __repr__(self):
+        # TODO: ( -> {, etc. in hands
+
         return (
-            f"State(turn={self.turn}, stock={self.stock}, trump={self.trump}, hands={self.hands}, "
-            f"score={self.score}, self.table={self.table}, is_endgame={self.is_endgame()}, winner={self.winner}"
+            f"State(turn={self.turn}, "
+            f"trump={self.trump}, "
+            f"hands=(South: {self.hands[Player.SOUTH]}, North: {self.hands[Player.NORTH]}), "
+            f"scores=(South: {self._scores[Player.SOUTH]}, North: {self._scores[Player.NORTH]}), "
+            f"table={self.table}, "
+            f"is_endgame={self.is_endgame()}, "
+            f"score={self.score}, "
+            f"winner={self.winner}, "
+            f"stock={self.stock})"
         )
 
-    # TODO: add save/load to json?
-
-    # TODO: add state.hand to State? (current hand property)
+    # TODO: load/save to json?
 
     @property
-    def opponent(self) -> Player:
-        return Player.NORTH if self.turn == Player.SOUTH else Player.SOUTH
+    def hand(self) -> List[Card]:
+        return self.hands[self.turn]
 
     @property
-    def winner(self) -> Optional[Winner]:
-        if self.is_endgame():
-            assert sum(self._scores.values()) == 120
-            # assert self.get_score(Player.SOUTH) == 120 - self.get_score(Player.NORTH)
+    def score(self) -> int:
+        # current score of winner, only valid for game at is_endgame
+        winner = self.winner
+        return (
+            self._scores[Player.NORTH]
+            if winner == Winner.DRAW
+            else self._scores[winner]
+        )
 
-            if self.score == 0:
-                return Winner.DRAW
-            else:
-                return Winner.SOUTH if self.score > 0 else Winner.NORTH
-        else:
-            return None
+    @property
+    def winner(self) -> Winner:
+        # current winner, only valid for game at is_endgame
+        if self._scores[Player.NORTH] > self._scores[Player.SOUTH]:
+            return Winner.NORTH
+        elif self._scores[Player.NORTH] < self._scores[Player.SOUTH]:
+            return Winner.SOUTH
+        else:  # ==
+            return Winner.DRAW
+
+    def copy(self) -> "State":
+        return copy.deepcopy(self)
 
     def do_rollout(self):
         # do random rollout
-        raise NotImplementedError  # TODO: implement this
-
-    def get_score(self, player: Player) -> int:
-        # human understandable score
-        return self._scores[player]
+        pass  # TODO: implement this
 
     def is_endgame(self) -> bool:
         return self._cards_in_stock_and_hands == 0
 
-    def play(self, move: Card) -> Optional[Tuple[Player, int]]:
-        # TODO: recheck game rules
-        # TODO: add other variants (that one that enforces follows after self.stock is empty)
+    @abstractmethod
+    def play(self, move: Card) -> Optional[Tuple[Player, int, List[Card]]]:
+        raise NotImplementedError
 
-        # TODO: wouldn't a better exception here would be IllegalMove?
+    def _deal_cards(self) -> None:
+        for turn in (self.turn, self.turn.opponent):
+            self.hands[turn].append(self.stock.pop())
+
+
+class StateVariant1(State):
+    # current implement Bisca variant - TODO: and yes, it needs a better name
+
+    def play(self, move: Card) -> Optional[Tuple[Player, int, List[Card]]]:
+        # TODO: recheck game rules
+
         self.hands[self.turn].remove(move)
         self._cards_in_stock_and_hands -= 1
 
-        # TODO: better format other than (player, card)? or separate in self.table and self.table_player?
+        # self.table could hold both, but that would allow the outside to glimpse implementation details
         self.table.append(move)
-        self._table_player.append(self.turn)
+        self._table_played.append(self.turn)
 
+        # after play
         if len(self.table) == 1:
-            # eldest
-            self.turn = self.opponent
+            # eldest played
+            self.turn = self.turn.opponent
         else:
-            # youngest
+            # youngest played
             winner = self._get_round_winner()
-            added_score = sum(card.score for card in self.table) * winner.value
-            self._scores[winner] += sum(card.score for card in self.table)
 
-            self.score += added_score
+            added_score = sum(card.score for card in self.table)
+            self._scores[winner] += added_score
+
             self.turn = winner
-            self.table = []
-            self._table_player = []
 
-            # if stock exists, deal cards -- TODO: put in deal cards? (ignores call if stock doesn't exist)
+            table = self.table
+            self.table = []
+            self._table_played = []
+
             if self.stock:
                 self._deal_cards()
 
-            return winner, added_score
+            return winner, added_score, table
 
         return None
 
-    def _deal_cards(self) -> None:
-        for turn in (self.turn, self.opponent):
-            self.hands[turn].append(self.stock.pop())
-
     def _get_round_winner(self) -> Player:
-        player1, player2 = self._table_player
         card1, card2 = self.table
-        # (player1, card1), (player2, card2) = self.table
-        return (
-            player1 if card1 > card2 else player2
-        )  # TODO: place to add follow variant (diff class?)
+        eldest, youngest = self._table_played
+
+        # follow is not mandatory, but if youngest plays a different suit than is not trump, eldest wins
+
+        if card1.suit != card2.suit:
+            if card2.is_trump():
+                return youngest
+            return eldest
+
+        # default
+        return eldest if card1 > card2 else youngest
