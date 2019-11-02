80f3ce81c322b79e1b3b3e58f73f42110dabab8f
Nuno Miguel Casteloa da Silva <NunoMCSilva@gmail.com>
2019-11-02 07:34:36 +0000

    Add Agent and RandomAgent

--- libbisca/__init__.py
+++ libbisca/__init__.py
@@ -3,6 +3,7 @@
 TODO: complete docstring
 """
 
+from libbisca.agents import Agent, RandomAgent
 from libbisca.card import Rank, Suit, Card, TrumpCard, Deck, get_card
 from libbisca.state import Player, Winner, State, StateVariant1
 
