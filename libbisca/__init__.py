6e1f78890a0252d811c521dc610801c46f64fc29
Nuno Miguel Casteloa da Silva <NunoMCSilva@gmail.com>
2019-11-02 08:35:10 +0000

    Add HandSize

--- libbisca/__init__.py
+++ libbisca/__init__.py
@@ -5,6 +5,6 @@ TODO: complete docstring
 
 from libbisca.agents import Agent, RandomAgent
 from libbisca.card import Rank, Suit, Card, TrumpCard, Deck, get_card
-from libbisca.state import Player, Winner, State, StateVariant1
+from libbisca.state import Player, Winner, HandSize, State, StateVariant1
 
 __version__ = "0.0.1"
