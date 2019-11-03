34ffaf41a53ec9e25483a4bbd06d62620b210852
Nuno Miguel Casteloa da Silva <NunoMCSilva@gmail.com>
2019-11-03 17:20:15 +0000

    Add reworked, simplified card and test_card

--- libbisca/card.py
+++ libbisca/card.py
@@ -1,208 +1,55 @@
-"""Card
-
-Implements Bisca card related classes.
-
-This module exports the following classes:
-    * Rank - enum representing all ranks supported by Bisca cards (Bisca doesn't support 8s, 9s, and 10s)
-    * Suit - enum representing all suits (Hearts, Diamonds, Spades, Clubs)
-    * Card - Bisca Card
-    * TrumpCard - Bisca Card marked as being a trump
-    * Deck - a game deck (a list of cards)
-"""
+# TODO: add docstrings
 
 from dataclasses import dataclass
-from enum import Enum, IntEnum, auto
-from random import SystemRandom
+from enum import Enum
 
 
-# Rank =================================================================================================================
-class Rank(IntEnum):
-    # sort order of ranks (which is greater)
-    TWO = auto()
-    THREE = auto()
-    FOUR = auto()
-    FIVE = auto()
-    SIX = auto()
-    QUEEN = auto()
-    JACK = auto()
-    KING = auto()
-    SEVEN = auto()
-    ACE = auto()
-
-    # TODO: recheck this, but right now it seems ok -- format used by f-strings
-    def __format__(self, format_spec):
-        # TODO: check format_spec
-        return str(self)
-
-    def __str__(self):
-        # TODO: any use to put "23... in a _STR class constant?
-        return "23456QJK7A"[self.value - 1]
-
-    @staticmethod
-    def get_rank(rank_str):
-        return Rank._STR_TO_RANK[rank_str]  # TODO: check this PyCharm warning
+class Rank(Enum):
+    # actual sort order
+    TWO = "2"
+    THREE = "3"
+    FOUR = "4"
+    FIVE = "5"
+    SIX = "6"
+    QUEEN = "Q"
+    JACK = "J"
+    KING = "K"
+    SEVEN = "7"
+    ACE = "A"
 
 
-# there are better solutions to putting constants in enums, but this one if good enough for now
-Rank._STR_TO_RANK = {
-    "A": Rank.ACE,
-    "2": Rank.TWO,
-    "3": Rank.THREE,
-    "4": Rank.FOUR,
-    "5": Rank.FIVE,
-    "6": Rank.SIX,
-    "7": Rank.SEVEN,
-    "Q": Rank.QUEEN,
-    "J": Rank.JACK,
-    "K": Rank.KING,
-}
-Rank.DECK_ORDER = (
-    Rank.ACE,
-    Rank.TWO,
-    Rank.THREE,
-    Rank.FOUR,
-    Rank.FIVE,
-    Rank.SIX,
-    Rank.SEVEN,
-    Rank.QUEEN,
-    Rank.JACK,
-    Rank.KING,
-)
-
-# Suit =================================================================================================================
 class Suit(Enum):
-    HEARTS = "♥"
-    DIAMONDS = "♦"
-    SPADES = "♠"
-    CLUBS = "♣"
-
-    def __format__(self, format_spec):
-        # TODO: check format_spec
-        return self.value
-
-    def __str__(self):
-        return self.value
-
-    @property
-    def trumped(self):
-        return self._TRUMPED[self]
-
-    @staticmethod
-    def get_suit(suit_str):
-        return Suit._STR_TO_SUIT[suit_str]  # TODO: check this PyCharm warning
+    HEARTS = "H"
+    DIAMONDS = "D"
+    SPADES = "S"
+    CLUBS = "C"
 
 
-Suit._TRUMPED = {
-    Suit.HEARTS: "♡",
-    Suit.DIAMONDS: "♢",
-    Suit.SPADES: "♤",
-    Suit.CLUBS: "♧",
-}
-Suit._STR_TO_SUIT = {
-    "H": Suit.HEARTS,
-    "D": Suit.DIAMONDS,
-    "S": Suit.SPADES,
-    "C": Suit.CLUBS,
-}
-
-# Card =================================================================================================================
 @dataclass
 class Card:
-    # all cards not in _SCORE are worth 0 points
-    _SCORE = {Rank.QUEEN: 2, Rank.JACK: 3, Rank.KING: 4, Rank.SEVEN: 10, Rank.ACE: 11}
-
     rank: Rank
     suit: Suit
 
     def __gt__(self, other: "Card"):
-        # only gt is implemented, the others aren't really necessary
-        # this compares card strength only, other issues are the state's responsibility
-        if other.is_trump():
-            # if other is trump, then self > other is ALWAYS False
-            return False
-        else:
-            return self.rank > other.rank
+        # only gt is implemented, state doesn't need the others
+        # this compares card strength ONLY, other issues are the state's responsibility (e.g. trump)
+        return self.rank.value > other.rank.value
 
     def __repr__(self):
-        # this should be in __str__ but I prefer to see this when I print a list of cards
-        # "Although practicality beats purity." -- The Zen of Python
-        return f"{self.rank}{self.suit}"
+        return f"{self.rank.value}{self.suit.value}"
 
     @property
     def score(self) -> int:
-        # should probably be in rank, but with it here I save a call to self.rank.score...
-        return Card._SCORE.get(self.rank, 0)
-
-    def trumpify(self) -> "TrumpCard":
-        return TrumpCard(self.rank, self.suit)
-
-    @staticmethod
-    def is_trump() -> bool:
-        return False
+        scores = {
+            k: v for k, v in zip(Rank, 5 * [0] + [2, 3, 4, 10, 11])
+        }  # put elsewhere
+        return scores[self.rank]
 
 
-# TrumpCard ============================================================================================================
-class TrumpCard(Card):
-    # simplifies card comparision without the need to modify each card to set trump (and it looks good on print)
-
-    def __gt__(self, other: "Card"):
-        # only gt is implemented, the others aren't really necessary
-        # this compares card strength only, other issues are the state's responsibility
-        if other.is_trump():
-            return self.rank > other.rank
-        else:
-            # self(t) > other, always
-            return True
-
-    def __repr__(self):
-        # this should be in __str__ but I prefer to see this when I print a list of cards
-        # "Although practicality beats purity." -- The Zen of Python
-        return f"{self.rank}{self.suit.trumped}"
-
-    @staticmethod
-    def is_trump() -> bool:
-        return True
-
-
-# Deck =================================================================================================================
-class Deck(list):
-    def __init__(self, shuffle=True):
-        deck = [Card(rank, suit) for suit in Suit for rank in Rank.DECK_ORDER]
-
-        # shuffle
-        if shuffle:
-            # SystemRandom is necessary to generate all of the 40! possibilities
-            SystemRandom().shuffle(deck)
-
-        # signal trumps -- TODO: make this more elegant
-        def func(card: Card) -> Card:
-            if card.suit != deck[0].suit:
-                return card
-            else:
-                return card.trumpify()
-
-        super().__init__(map(func, deck))
-
-    @property
-    def bottom(self) -> Card:
-        # bottom of deck
-        return self[0]
-
-    def take_top(self) -> Card:
-        # take card from top of deck
-        return self.pop()
-
-
-# functions ============================================================================================================
 def get_card(card_str: str) -> Card:
-    # mostly for testing purposes - get_card("2H") is easier to type than Card(Rank.TWO, Suit.HEARTS)
-
-    card_cls = Card
-    if "(t)" in card_str:
-        card_cls = TrumpCard
-        card_str = card_str[0:2]
+    # put at module level or just generate _STR_TO_CARDS
+    ranks = {rank.value: rank for rank in Rank}
+    suits = {suit.value: suit for suit in Suit}
 
     rank, suit = card_str
-    return card_cls(
-        Rank.get_rank(rank), Suit.get_suit(suit)
-    )  # TODO: check this PyCharm warning
+    return Card(ranks[rank], suits[suit])
--- tests/unit/test_card.py
+++ tests/unit/test_card.py
@@ -5,94 +5,37 @@ from libbisca.card import *
 # using Roy Osherove's [UnitOfWork_StateUnderTest_ExpectedBehavior] unittest naming
 
 
-class TestRank:
-    def test__check_order__return_expected(self):
-        # this is more for my peace of mind than anything else
-        # TODO: check PyCharm typing warning in next line
-        assert (
-            "".join(map(str, Rank)) == "".join(map(str, sorted(Rank))) == "23456QJK7A"
-        )
-
-    def test__gt__seven_gt_two__return_true(self):
-        assert Rank.SEVEN > Rank.TWO
-
-    def test__str_and_format__use_of_str_and_f_string__return_expected_char(self):
-        assert str(Rank.SEVEN) == f"{Rank.SEVEN}" == "7"
-
-    def test__get_rank__any_rank_str__returns_expected_rank(self):
-        assert Rank.get_rank("Q") == Rank.QUEEN
-
-
-class TestSuit:
-    def test__str_and_format__use_of_str_and_f_string__return_expected_char(self):
-        assert str(Suit.HEARTS) == f"{Suit.HEARTS}" == "♥"
-
-    def test__trumped__any_suit__return_expected_char(self):
-        assert Suit.HEARTS.trumped == "♡"
-
-    def test__get_suit__any_suit_str__returns_expected_suit(self):
-        assert Suit.get_suit("C") == Suit.CLUBS
-
-
 class TestCard:
-    def test__repr__any_card__return_expected_str(self):
-        assert repr(Card(Rank.THREE, Suit.HEARTS)) == "3♥"
-
-    @pytest.mark.parametrize(
-        "card_str, score", [("7S", 10), ("QH", 2), ("2C", 0), ("5D", 0)]
-    )
-    def test__score__any_card__return_expected_value(self, card_str, score):
-        assert get_card(card_str).score == score
-
-
-class TestTrumpCard:
-    def test__repr__any_card__return_expected_str(self):
-        assert repr(TrumpCard(Rank.ACE, Suit.SPADES)) == "A♤"
-
-
-class TestCardAndTrumpCard:
-    @pytest.mark.parametrize(
-        "card1, card2, expected",
-        [
-            # same type, same suit: compare rank
-            ("AH", "7H", True),  # AH > 7H
-            ("AH(t)", "7H(t)", True),  # AH(t) > 7H(t)
-            ("5C", "7C", False),  # 5C < 7C
-            ("5C(t)", "7C(t)", False),  # 5C(t) < 7C(t)
-            # same type, different suits: still compare rank (the not following part is state's responsibility)
-            ("AC", "7H", True),  # AC > 7H
-            ("AC(t)", "7H(t)", True),  # AC(t) > 7H(t)
-            ("6D", "7C", False),  # 6D < 7C
-            ("6D(t)", "7C(t)", False),  # 6D(t) < 7C(t)
-            # different type
-            ("2D(t)", "7C", True),  # 2D(t) > 7C
-            ("3D", "2S(t)", False),  # 3D < 2S(t)
-        ],
-    )
-    def test__gt_two_cards__return_expected_bool(self, card1, card2, expected):
+    @pytest.mark.parametrize("cards", ["2H < 5H", "6C > 3C", "3S < 7S", "5H > 4C"])
+    def test__lt__x__x(self, cards):
         # arrange
-        c1 = get_card(card1)
-        c2 = get_card(card2)
+        c1, comp, c2 = cards.split(" ")
+        c1, c2 = [get_card(c) for c in (c1, c2)]
+        expected = True if comp == "<" else False
+
+        # act & Assert
+        assert (c1 < c2) is expected
+
+    def test__repr__any_card__return_expected(self):
+        # mostly for my peace of mind
+
+        # arrange
+        card = Card(Rank.QUEEN, Suit.SPADES)
 
         # act & assert
-        assert (c1 > c2) is expected
+        assert repr(card) == "QS"
 
-
-class TestDeck:
-    def test__non_shuffled_deck__returns_expected_deck(self):
+    def test__score__any_card__return_expected(self):
         # arrange
-        expected = (
-            "[A♡, 2♡, 3♡, 4♡, 5♡, 6♡, 7♡, Q♡, J♡, K♡, A♦, 2♦, 3♦, 4♦, 5♦, 6♦, 7♦, Q♦, J♦, K♦, A♠, 2♠, 3♠, 4♠, 5♠,"
-            " 6♠, 7♠, Q♠, J♠, K♠, A♣, 2♣, 3♣, 4♣, 5♣, 6♣, 7♣, Q♣, J♣, K♣]"
-        )
+        card = Card(Rank.QUEEN, Suit.SPADES)
 
         # act & assert
-        assert str(Deck(shuffle=False)) == expected
+        assert card.score == 2
 
 
-class TestGetCard:
-    def test__any_card_str__return_expected_card(self):
-        assert get_card("JS") == Card(Rank.JACK, Suit.SPADES)
+def test__get_card__any_card_str__return_expected():
+    # arrange
+    card = Card(Rank.QUEEN, Suit.SPADES)
 
-    def test__any_trump_card_str__return_expected_card(self):
-        assert get_card("JS(t)") == TrumpCard(Rank.JACK, Suit.SPADES)
+    # act & assert
+    assert get_card("QS") == card
