ece66cfb12a16a981e1e6ab4b56fe452e3a31831
Nuno Miguel Casteloa da Silva <NunoMCSilva@gmail.com>
2019-11-07 07:05:25 +0000

    Lots of corrections

--- tests/unit/test_card.py
+++ tests/unit/test_card.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import pytest
 
 from libbisca.card import *
@@ -6,63 +8,78 @@ from libbisca.card import *
 
 
 class TestCard:
-    DECK = [
-        Card.get_card(s)
-        for s in "AH 2H 3H 4H 5H 6H 7H QH JH KH "
-        "AD 2D 3D 4D 5D 6D 7D QD JD KD "
-        "AS 2S 3S 4S 5S 6S 7S QS JS KS "
-        "AC 2C 3C 4C 5C 6C 7C QC JC KC".split(" ")
-    ]
-
     @pytest.mark.parametrize("cards", ["2H < 5H", "6C > 3C", "3S < 7S", "5H > 4C"])
-    def test__lt__two_cards__returns_expected(self, cards):
+    def test__gt__two_cards__returns_expected(self, cards):
         # arrange
-        c1, comp, c2 = cards.split(" ")
-        c1, c2 = [Card.get_card(c) for c in (c1, c2)]
-        expected = True if comp == "<" else False
+        c1, op, c2 = cards.split(" ")
+        c1, c2 = get_cards(cards.replace(op, "").replace("  ", " "))
+        expected = (op == ">")
 
         # act & Assert
-        assert (c1 < c2) is expected
+        assert (c1 > c2) is expected
 
-    def test__repr__any_card__returns_expected(self):
+    @pytest.mark.parametrize("card_str", ["QS", "KH", "2D"])
+    def test__repr__any_card__returns_expected(self, card_str):
         # mostly for my peace of mind
 
         # arrange
-        card = Card(Rank.QUEEN, Suit.SPADES)
+        card = get_card(card_str)
 
         # act & assert
-        assert repr(card) == "QS"
+        assert isinstance(card, Card)  # again, peace of mind
+        assert repr(card) == card_str
 
     @pytest.mark.parametrize(
         "card_str, score", [("7S", 10), ("QH", 2), ("2C", 0), ("5D", 0)]
     )
     def test__score__any_card__returns_expected(self, card_str, score):
         # arrange
-        card = Card.get_card(card_str)
+        card = get_card(card_str)
 
         # act & assert
         assert card.score == score
 
-    def test__get_card__any_card_str__returns_expected(self):
+
+class TestDeck:
+    DECK_STR = (
+        "AH 2H 3H 4H 5H 6H 7H QH JH KH AD 2D 3D 4D 5D 6D 7D QD JD KD "
+        "AS 2S 3S 4S 5S 6S 7S QS JS KS AC 2C 3C 4C 5C 6C 7C QC JC KC"
+    )
+    DECK = get_cards(DECK_STR)  # list
+
+    def test__init__not_shuffled__returns_expected(self):
+        # act & assert
+        assert Deck(shuffle=False) == TestDeck.DECK
+
+    def test__init__shuffled__returns_expected(self):
+        # act
+        deck = Deck()
+
+        # act & assert
+        assert deck != TestDeck.DECK
+        # I'd prefer sorted but result is unpredictable (ranks correct, suit random) - no point in worrying about this
+        assert set(deck) == set(TestDeck.DECK)
+        assert len(set(deck)) == len(deck)  # ok, now this is just paranoia
+
+
+class TestGetCard:
+
+    def test__any_card_str__returns_expected(self):
         # arrange
         card = Card(Rank.QUEEN, Suit.SPADES)
+        card_str = "QS"
 
         # act & assert
-        assert Card.get_card("QS") == card
+        assert get_card(card_str) == card
 
-    def test__get_deck__not_shuffled__returns_expected(self):
+    def test__any_cards_str__returns_expected(self):
+        # arrange
+        cards = [
+            Card(Rank.QUEEN, Suit.SPADES),
+            Card(Rank.KING, Suit.HEARTS),
+            Card(Rank.SEVEN, Suit.CLUBS),
+        ]
+        cards_str = "QS KH 7C"
+
         # act & assert
-        assert Card.get_deck(shuffle=False) == TestCard.DECK
-
-    def test__get_deck__shuffled__x(self):
-        # act
-        deck = Card.get_deck()
-
-        # act & assert
-        assert deck != TestCard.DECK
-        assert set(deck) == set(
-            TestCard.DECK
-        )  # sorted is uncertain here - unimportant for now
-
-
-# TODO: add test for get_cards
+        assert get_cards(cards_str) == cards
