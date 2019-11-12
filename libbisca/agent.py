358dc90ce865f32ec12b10feac56161a25ea55f4
Nuno Miguel Casteloa da Silva <NunoMCSilva@gmail.com>
2019-11-12 16:28:06 +0000

    Add comments

--- libbisca/agent.py
+++ libbisca/agent.py
@@ -18,12 +18,10 @@ class Agent(ABC):
     # abstract base class
 
     @abstractmethod
-    def get_move(self, state) -> Card:      # TODO: see ObservedState option, typing "State"
+    def get_move(self, state) -> Card:  # TODO: see ObservedState option, typing "State"
         raise NotImplementedError
 
 
 class RandomAgent(Agent):
-    # this might make the code simpler
-
-    def get_move(self, state) -> Card:      # TODO: typing "State"
+    def get_move(self, state) -> Card:  # TODO: typing "State"
         return random.choice(state.legal_moves)
