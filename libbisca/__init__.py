644e3cee98fa41836fda24289f42d660e2529605
Nuno Miguel Casteloa da Silva <NunoMCSilva@gmail.com>
2019-11-12 09:32:44 +0000

    Add agent, modify state

--- libbisca/__init__.py
+++ libbisca/__init__.py
@@ -1,4 +1,5 @@
 # TODO: add docstrings
 
+from libbisca.agent import Agent, RandomAgent
 from libbisca.card import Card, Deck, Rank, Suit
-from libbisca.state import Player, State, Variant, Winner, get_state
+from libbisca.state import Player, State, Winner, get_state, run_games
