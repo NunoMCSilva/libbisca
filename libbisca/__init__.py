93b5ebe0a0b446f19c6c2654b916acbd12deb618
Nuno Miguel Casteloa da Silva <NunoMCSilva@gmail.com>
2019-11-13 23:41:38 +0000

    Reworking

--- libbisca/__init__.py
+++ libbisca/__init__.py
@@ -2,6 +2,9 @@
 
 # TODO: add docstrings
 
+# TODO: decide what to expose as API
 from libbisca.agent import Agent, RandomAgent
-from libbisca.card import Card, Deck, Rank, Suit
-from libbisca.state import Player, State, Winner, get_state, run_games
+from libbisca.card import Deck  # Card, Deck, Rank, Suit, get_card, get_cards
+from libbisca.state import get_state  # Player, State, load_state
+
+__version__ = "0.1.0"
