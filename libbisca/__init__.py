74bdd3cb8778c347563d4eda97ddac0f308239b2
Nuno Miguel Casteloa da Silva <NunoMCSilva@gmail.com>
2019-11-13 23:57:32 +0000

    Correction to version

--- libbisca/__init__.py
+++ libbisca/__init__.py
@@ -7,4 +7,4 @@ from libbisca.agent import Agent, RandomAgent
 from libbisca.card import Deck  # Card, Deck, Rank, Suit, get_card, get_cards
 from libbisca.state import get_state  # Player, State, load_state
 
-__version__ = "0.1.0"
+__version__ = "0.0.1"
