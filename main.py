8bcd68e503843444557580a9a7e3003d24dc914d
Nuno Miguel Casteloa da Silva <NunoMCSilva@gmail.com>
2019-11-14 00:10:09 +0000

    Add some examples

--- main.py
+++ main.py
@@ -5,15 +5,22 @@ from libbisca import Deck, get_state
 
 def main():
     d = Deck()
-    print(d)
+    print("deck:", d)
     print()
 
     s = get_state()
-    print(s)
+    print("init state:", s)
     print()
 
     s.do_random_rollout()
-    print(s)
+    print("termianl state:", s)
+    print()
+
+    s = get_state()
+    while not s.is_endgame():
+        print("state:", s)
+        print("move, result:", s.do_random_move())
+    print("state:", s)
     print()
 
 
